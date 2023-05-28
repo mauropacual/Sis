75a445a72101ebb1082794b83c406d7c77f2d8b0
Mauro Pascual Rivera <riveramauro278@gmail.com>
2023-05-28 16:26:00 -0400

    aa

--- AppSis/data/models/user.py
+++ AppSis/data/models/user.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
+from sqlalchemy.orm import relationship
 from data import Base
 
 class User(Base):
@@ -9,3 +10,14 @@ class User(Base):
     last_name = Column(String(30), nullable=False)
     email = Column(String(60), nullable=False)
     password = Column(String(20), nullable=False)
+    user_type = Column(String(20), nullable=False)
+    role = Column(String(4), ForeignKey('role.id_role'))
+
+    role_info = relationship('Role', back_populates='users')
+    user_permissions = relationship(
+        'Permission', secondary='user_permission', back_populates='users')
+    
+    __mapper_args__ = {
+        'polymorphic_on': user_type,
+        'polymorphic_identity': 'user'
+    }
